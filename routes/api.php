f35d0572daeb8e4d94ae53f6298cfc149b6416ef
aliya <92076520+aliya25@users.noreply.github.com>
2022-10-30 17:06:20 +0700

    author

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,13 +23,30 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('me', [AuthController::class, 'me']);
 
-Route::get('/books', [BookController::class, 'index']);
-Route::get('/books/{id}', [BookController::class, 'show']);
-Route::post('/books', [BookController::class, 'store']);
-Route::put('/books/(id}', [BookController::class, 'update']);
-Route::delete('/books/{id}', [BookController::class]);
+//Route::get('/books', [BookController::class, 'index']);
+//Route::get('/books/{id}', [BookController::class, 'show']);
+//Route::post('/books', [BookController::class, 'store']);
+//Route::put('/books/{id}', [BookController::class, 'update']);
+//Route::delete('/books/{id}', [BookController::class]);
 
-Route::resource('books', BookController::class)->except(
-    ['create','edit']
+//Route::resource('books', BookController::class)->except(
+//    ['create','edit']
+//);
+//Route::delete('books/{id}', [BookController::class, 'destroy']);
+//Route::resource('author', AuthorController::class)->except(
+//    ['create','edit']
+//);    
+//Route::get('/author', [AuthorController::class, 'index']);
+//Route::get('/author/{id}', [AuthorController::class, 'show']);
+//Route::post('/author', [AuthorController::class, 'store']);
+//Route::put('/author/{id}', [AuthorController::class, 'update']);
+//Route::delete('/author/{id}', [AuthorController::class]);
+
+Route::resource('author', AuthorController::class)->except(
+    ['create', 'edit']
 );
-Route::delete('books/{id}', [BookController::class, 'destroy']);
+Route::get('author', [AuthorController::class, 'index']);
+Route::get('author/{id}', [AuthorController::class, 'show']);//detail
+Route::post('author', [AuthorController::class, 'store']);
+Route::put('author/{id}', [AuthorController::class, 'update']);
+Route::delete('/author/{id}', [AuthorController::class, 'destroy']);
